2239a8ffcfe9b3e9ac77b47edfd44df12ef235d8
SantuHB <santhosh.proxy@gmail.com>
2020-04-15 18:51:34 +0530

    Project Files Committed

---
+++ Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args)
+	{
+	System.out.println("WELCOME");
+	System.out.println("JDK Version : " +System.getProperty("java.version"));
+    	System.out.println("JRE Version : " +System.getProperty("java.runtime.version"));
+    
+
+
+	}
+
+}
